8a8ac1225aedba62e245bcf81a3e9810ec5f444c
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-06 23:53:15 +0600

    Updates

---
+++ precache-manifest.239b8a1efdfe83ae0580bb89b46bc4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a264f51de6b84eaed287315bc90c83",
+    "url": "/react_reviews/index.html"
+  },
+  {
+    "revision": "72a3858135247da2702f",
+    "url": "/react_reviews/static/css/main.714a7fac.chunk.css"
+  },
+  {
+    "revision": "a5d7eeb8600de74adb4f",
+    "url": "/react_reviews/static/js/2.5945611d.chunk.js"
+  },
+  {
+    "revision": "6673e90349f6cb4629a772fd2c7c7727",
+    "url": "/react_reviews/static/js/2.5945611d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a3858135247da2702f",
+    "url": "/react_reviews/static/js/main.5f644575.chunk.js"
+  },
+  {
+    "revision": "7d551905e5d0f2a2d3f7",
+    "url": "/react_reviews/static/js/runtime-main.882ef762.js"
+  }
+]);
