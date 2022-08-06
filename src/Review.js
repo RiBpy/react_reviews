fa5ed510ae65dc801526365ecf2a99fc3ab29809
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-06 23:23:27 +0600

    source code added

---
+++ src/Review.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import people from './data';
+const Review = () => {
+  //hooks
+  const [index,setIndex]=useState(0);
+  const {name,job,text,image}=people[index];
+  //check if incoming index is in array of data
+  const checkNumber=(number)=>{
+    if(number>people.length-1){
+      return 0
+    }else if(number<0){
+      return people.length-1;
+    }else{
+      return number
+    }
+    }
+  //prev and next  button 
+  const prevReview=()=>{
+    setIndex(index=>{let newIndex=index-1; return checkNumber(newIndex)})
+  }
+  const nextReview=()=>{
+    setIndex(index=>{let newIndex=index+1; return checkNumber(newIndex)})
+  }
+
+//review randomly..means review will come not following any sequence 
+const randomPerson =()=>{
+  let randomNumber=Math.floor(Math.random()*people.length)//random number between 0 to array elements
+  if (randomNumber===index){ //same random number will not come
+    randomNumber=index+1;
+  }
+  setIndex(checkNumber(randomNumber))
+}
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className="autho">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevReview}>
+          <FaChevronLeft/>
+        </button>
+        <button className="prev-btn "onClick={nextReview}>
+          <FaChevronRight/>
+        </button>
+      </div>
+    </article>
+  )
+};
+
+export default Review;
---
+++ src/data.js
@@ -0,0 +1,88 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 5,
+    name: 'Ice Cream',
+    job: 'the boss',
+    image:"https://foodish-api.herokuapp.com/images/dessert/dessert14.jpg",
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 6,
+    name: 'Biriyani',
+    job: 'the boss',
+    image:"https://foodish-api.herokuapp.com/images/biryani/biryani17.jpg",
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 7,
+    name: 'Rice',
+    job: 'the boss',
+    image:"https://foodish-api.herokuapp.com/images/rice/rice21.jpg",
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 8,
+    name: 'Samosa',
+    job: 'Developer',
+    image:"https://foodish-api.herokuapp.com/images/samosa/samosa7.jpg",
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 9,
+    name: 'Pasta',
+    job: 'Designer',
+    image:"https://foodish-api.herokuapp.com/images/pasta/pasta4.jpg",
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 10,
+    name: 'Pizza',
+    job: 'Front end developer',
+    image:'https://foodish-api.herokuapp.com/images/pizza/pizza16.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+export default reviews;
